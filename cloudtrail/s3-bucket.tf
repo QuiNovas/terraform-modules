07d3890942820bbaed9500240ca19db27eb4e078
Joseph Wortmann <joseph.wortmann@gmail.com>
2017-07-15 19:48:28 -0500

    Added a cloudtrail module for use in accounts

---
+++ cloudtrail/s3-bucket.tf
@@ -0,0 +1,70 @@
+resource "aws_s3_bucket" "cloudtrail" {
+  acl     = "log-delivery-write"
+  bucket  = "${var.account_name}-cloudtrail"
+
+  lifecycle_rule {
+    id = "log"
+    prefix = "/"
+    enabled = true
+
+    transition {
+      days = 30
+      storage_class = "GLACIER"
+    }
+
+    expiration {
+      days = 2555
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+data "aws_iam_policy_document" "cloudtrail_s3" {
+
+  statement {
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    principals {
+      identifiers = [
+        "cloudtrail.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    resources = [
+      "${aws_s3_bucket.cloudtrail.arn}"
+    ]
+    sid = "CloudTrail Acl Check"
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    condition {
+      test = "StringEquals"
+      values = [
+        "bucket-owner-full-control"
+      ]
+      variable = "s3:x-amz-acl"
+    }
+    principals {
+      identifiers = [
+        "cloudtrail.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    resources = [
+      "${aws_s3_bucket.cloudtrail.arn}/*"
+    ]
+    sid = "CloudTrail Write"
+  }
+}
+
+resource "aws_s3_bucket_policy" "cloudtrail" {
+  bucket = "${aws_s3_bucket.cloudtrail.id}"
+  policy = "${data.aws_iam_policy_document.cloudtrail_s3.json}"
+}
---
+++ cloudtrail/sns-topic.tf
@@ -0,0 +1,27 @@
+resource "aws_sns_topic" "cloudtrail" {
+  name = "${var.account_name}-cloudtrail"
+}
+
+data "aws_iam_policy_document" "cloudtrail_sns" {
+  statement {
+    actions = [
+      "sns:Publish"
+    ]
+    principals {
+      identifiers = [
+        "cloudtrail.amazonaws.com"
+      ]
+      type = "Service"
+    }
+    resources = [
+      "${aws_sns_topic.cloudtrail.arn}"
+    ]
+    sid = "CloudTrail SNS Policy"
+  }
+}
+
+resource "aws_sns_topic_policy" "cloudtrail" {
+  arn = "${aws_sns_topic.cloudtrail.arn}"
+  policy = "${data.aws_iam_policy_document.cloudtrail_sns.json}"
+}
+
