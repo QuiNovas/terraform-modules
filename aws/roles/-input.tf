42fae47e148e57f21130e81669cdd6f05ad8ff88
Joseph Wortmann <joseph.wortmann@gmail.com>
2017-08-07 21:33:16 -0500

    big update to fit policies in 2048 limit

--- aws/roles/-input.tf
+++ aws/roles/-input.tf
@@ -1,3 +1,7 @@
 variable "terraform_allowed_user_arns" {
   type = "list"
 }
+
+variable "terraform_allowed_user_names" {
+  type = "list"
+}
--- aws/roles/terraform/config.tf
+++ aws/roles/terraform/config.tf
@@ -26,3 +26,43 @@ resource "aws_iam_role_policy_attachment" "terraform" {
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   role = "${aws_iam_role.terraform.name}"
 }
+
+resource "aws_iam_group" "terraform" {
+  name = "terraform"
+}
+
+data "aws_iam_policy_document" "terraform" {
+  "statement" {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    resources = [
+      "${aws_iam_role.terraform.arn}"
+    ]
+    sid = "AllowAssumingOfRole"
+  }
+
+  statement {
+    actions = [
+      "iam:ListAttachedRolePolicies"
+    ]
+    resources = [
+      "${aws_iam_role.terraform.arn}"
+    ]
+    sid = "AllowListingOfRolePolicies"
+  }
+}
+
+resource "aws_iam_group_policy" "terraform" {
+  group   = "${aws_iam_group.terraform.id}"
+  name    = "role_access"
+  policy  = "${data.aws_iam_policy_document.terraform.json}"
+}
+
+resource "aws_iam_group_membership" "terraform" {
+  group = "${aws_iam_group.terraform.name}"
+  name  = "terraform-membership"
+  users = [
+    "${var.allowed_user_names}"
+  ]
+}
