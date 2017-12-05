b85d93ac11b0169918a4530c485c6f0c758391b4
Joseph Wortmann <joseph.wortmann@gmail.com>
2017-12-04 21:45:42 -0600

    added cloudwatch logging to cloudtrail

--- aws/cloudtrail/config.tf
+++ aws/cloudtrail/config.tf
@@ -1,4 +1,6 @@
 resource "aws_cloudtrail" "cloudtrail" {
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}"
+  cloud_watch_logs_role_arn     = "${aws_iam_role.cloudwatch_logs.arn}"
   enable_log_file_validation    = true
   include_global_service_events = true
   is_multi_region_trail         = true
