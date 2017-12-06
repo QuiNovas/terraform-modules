11890913b7acb7a5e70e2a28e75be85a19cccc1d
Joseph Wortmann <joseph.wortmann@gmail.com>
2017-12-06 10:32:24 -0600

    Another rules package

--- aws/inspector/linux.tf
+++ aws/inspector/linux.tf
@@ -16,6 +16,7 @@ resource "aws_inspector_assessment_template" "linux" {
   rules_package_arns  = [
     "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
     "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gBONHN9h",
+    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7"
   ]
   target_arn          = "${aws_inspector_assessment_target.windows.arn}"
 }
