48a21c47fd851b09fdd507dedec1d3588743021c
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-01-15 11:35:41 -0600

    Forgot to add the log for the lambda function

--- aws/cloudfront/default-index-redirect.tf
+++ aws/cloudfront/default-index-redirect.tf
@@ -13,9 +13,33 @@ data "aws_iam_policy_document" "redirector_assume_role" {
   }
 }
 
-resource "aws_iam_role" "redirector_assume_role" {
+resource "aws_iam_role" "redirector" {
   assume_role_policy  = "${data.aws_iam_policy_document.redirector_assume_role.json}"
-  name                = "${var.distribution_name}"
+  name                = "${var.distribution_name}-default-index-redirector"
+}
+
+resource "aws_cloudwatch_log_group" "redirector_log_group" {
+  name              = "/aws/lambda/${var.distribution_name}-default-index-redirector"
+  retention_in_days = 7
+}
+
+data "aws_iam_policy_document" "redirector" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = [
+      "${aws_cloudwatch_log_group.redirector_log_group.arn}"
+    ]
+    sid       = "AllowLogCreation"
+  }
+}
+
+resource "aws_iam_role_policy" "redirector" {
+  name    = "${var.distribution_name}-default-index-redirector"
+  policy  = "${data.aws_iam_policy_document.redirector.json}"
+  role    = "${aws_iam_role.redirector.id}"
 }
 
 resource "aws_lambda_function" "redirector" {
@@ -23,7 +47,7 @@ resource "aws_lambda_function" "redirector" {
   function_name = "${var.distribution_name}-default-index-redirector"
   handler       = "exports.handler"
   publish       = true
-  role          = "${aws_iam_role.redirector_assume_role.arn}"
+  role          = "${aws_iam_role.redirector.arn}"
   runtime       = "nodejs6.10"
 }
 
