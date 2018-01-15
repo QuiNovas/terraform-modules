33cceb59c1462e88da865bb73958b4c27de23814
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-01-15 09:38:05 -0600

    added an actual role

--- aws/cloudfront/default-index-redirect.tf
+++ aws/cloudfront/default-index-redirect.tf
@@ -12,12 +12,17 @@ data "aws_iam_policy_document" "redirector_assume_role" {
   }
 }
 
+resource "aws_iam_role" "redirector_assume_role" {
+  assume_role_policy  = "${data.aws_iam_policy_document.redirector_assume_role.json}"
+  name                = "${var.distribution_name}"
+}
+
 resource "aws_lambda_function" "redirector" {
   filename      = "${path.module}/default-index-redirect.zip"
   function_name = "${var.distribution_name}-default-index-redirector"
   handler       = "exports.handler"
   publish       = true
-  role          = "${data.aws_iam_policy_document.redirector_assume_role.json}"
+  role          = "${aws_iam_role.redirector_assume_role.arn}"
   runtime       = "nodejs6.10"
 }
 
