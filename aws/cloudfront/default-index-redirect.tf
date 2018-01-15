f901f626ce313f90781daf3cf78d6072fe9abed8
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-01-15 12:00:53 -0600

    update lambda function again

--- aws/cloudfront/default-index-redirect.tf
+++ aws/cloudfront/default-index-redirect.tf
@@ -43,12 +43,13 @@ resource "aws_iam_role_policy" "redirector" {
 }
 
 resource "aws_lambda_function" "redirector" {
-  filename      = "${path.module}/default-index-redirect.zip"
-  function_name = "${var.distribution_name}-default-index-redirector"
-  handler       = "exports.handler"
-  publish       = true
-  role          = "${aws_iam_role.redirector.arn}"
-  runtime       = "nodejs6.10"
+  filename          = "${path.module}/default-index-redirect/function.zip"
+  function_name     = "${var.distribution_name}-default-index-redirector"
+  handler           = "exports.handler"
+  publish           = true
+  role              = "${aws_iam_role.redirector.arn}"
+  runtime           = "nodejs6.10"
+  source_code_hash  = "${base64sha256(file("${path.module}/default-index-redirect/function.zip"))}"
 }
 
 resource "aws_lambda_permission" "redirector" {
--- aws/cloudfront/default-index-redirect/build-package.sh
+++ aws/cloudfront/default-index-redirect/build-package.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 npm install async
-zip -r ../default-index-redirect.zip *
+zip -r function.zip *
