e3c0f76c07417b257d0b22630a408502b6ce3015
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-01-19 10:45:33 -0600

    Added a module to create a rt53 zone
    and all the NON-alias records

---
+++ aws/route53/public-zone/-input.tf
@@ -0,0 +1,16 @@
+variable "delegation_set_id" {
+  type    = "string"
+  default = ""
+}
+
+variable "name" {
+  type = "string"
+}
+
+variable "record_set" {
+  type = "list"
+}
+
+variable "record_set_count" {
+  type = "string"
+}
