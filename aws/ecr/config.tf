534dad75bcd286acba916190cc26bfebae3a3b1b
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-02-05 09:34:52 -0600

    updated json

--- aws/ecr/config.tf
+++ aws/ecr/config.tf
@@ -69,10 +69,10 @@ resource "aws_ecr_lifecycle_policy" "repo" {
           "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
           "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
           "s", "t", "u", "v", "w", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
-        ]
+        ],
         "tagStatus": "tagged"
       }
-    },
+    }
   ]
 }
 POLICY
