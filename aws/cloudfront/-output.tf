2a7014b622bd378833d228bee0efcb7a00a575d5
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-03-22 21:10:41 -0500

    output the arn

--- aws/cloudfront/-output.tf
+++ aws/cloudfront/-output.tf
@@ -1,3 +1,7 @@
+output "distribution_arn" {
+  value = "${aws_cloudfront_distribution.distribution.arn}"
+}
+
 output "distribution_id" {
   value = "${aws_cloudfront_distribution.distribution.id}"
 }
