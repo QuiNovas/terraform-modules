1b91f7a947243be65bd66b2d216791e9a1a77469
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-06-11 09:40:15 -0500

    tried to make sure of a unique name

--- aws/cloudfront/origin-bucket.tf
+++ aws/cloudfront/origin-bucket.tf
@@ -1,5 +1,15 @@
+resource "random_string" "origin_suffix" {
+  keepers {
+    keep = true
+  }
+  length  = 8
+  lower   = false
+  special = false
+  upper   = false
+}
+
 resource "aws_s3_bucket" "origin" {
-  bucket = "${var.distribution_name}"
+  bucket = "${var.distribution_name}-${random_string.origin_suffix.result}"
   lifecycle {
     prevent_destroy = true
   }
