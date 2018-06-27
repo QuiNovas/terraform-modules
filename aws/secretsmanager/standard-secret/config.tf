d206b4236f95d61f880a15a1f28aa07527fc5717
Vinay Kumar Chinnakotla <vchinnakotla@quinovas.com>
2018-06-27 16:26:19 +0530

    add secret string value

--- aws/secretsmanager/standard-secret/config.tf
+++ aws/secretsmanager/standard-secret/config.tf
@@ -14,3 +14,8 @@ resource "aws_secretsmanager_secret" "secret" {
   name        = "${var.name}"
   tags        = "${var.tags}"
 }
+
+resource "aws_secretsmanager_secret_version" "secret" {
+  secret_id     = "${aws_secretsmanager_secret.secret.id}"
+  secret_string = ""
+}
