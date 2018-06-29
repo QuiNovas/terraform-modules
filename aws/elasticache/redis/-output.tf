b3a4b51a1db42134bc75f71f82a85ca4c31c6e63
Joseph Wortmann <joseph.wortmann@gmail.com>
2018-06-29 11:42:27 -0500

    another output

--- aws/elasticache/redis/-output.tf
+++ aws/elasticache/redis/-output.tf
@@ -2,6 +2,10 @@ output "auth_token" {
   value = "${random_string.auth_token.result}"
 }
 
+output "primary_endpoint_address" {
+  value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
+}
+
 output "replication_group_id" {
   value = "${aws_elasticache_replication_group.redis.id}"
 }
